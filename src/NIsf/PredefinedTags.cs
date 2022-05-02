ae48c3c48f9f9e8e9cdbfd5287328b50444645ad
tk <kugelto@outlook.com>
2022-05-03 00:17:01 +0200

    initial commit

---
+++ src/NIsf/PredefinedTags.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIsf;
+public static class PredefinedTags
+{
+    public const byte IndexGuidX                = 0;
+    public const byte IndexGuidY                = 1;
+    public const byte IndexGuidZ                = 2;
+    public const byte IndexGuidPacketStatus     = 3;
+    public const byte IndexGuidTimerTick        = 4;
+    public const byte IndexGuidSerialNumber     = 5;
+    public const byte IndexGuidNormalPressure   = 6;
+    public const byte IndexGuidTangetPressure   = 7;
+    public const byte IndexGuidButtonPressure   = 8;
+    public const byte IndexGuidXTiltOrientation = 9;
+    public const byte IndexGuidYTiltOrientation = 10;
+    public const byte IndexGuidAzimuthOrientation   = 11;
+    public const byte IndexGuidAltitudeOrientation  = 12;
+    public const byte IndexGuidTwistOrientation = 13;
+    public const byte IndexGuidPitchOrientation = 14;
+    public const byte IndexGuidRollRotation = 15;
+    public const byte IndexGuidYawRotation  = 16;
+    public const byte IndexGuidPenStyle     = 17;
+    public const byte IndexGuidColorRef     = 18;
+    public const byte IndexGuidPenWidth     = 19;
+    public const byte IndexGuidPenHeight    = 20;
+    public const byte IndexGuidPenTip       = 21;
+    public const byte IndexGuidDrawingFlags = 22;
+    public const byte IndexGuidCursorId     = 23;
+    public const byte IndexGuidWordAlternates   = 24;
+    public const byte IndexGuidCharAlternates   = 25;
+    public const byte IndexGuidInkMetrics       = 26;
+    public const byte IndexGuidGuideStructure   = 27;
+    public const byte IndexGuidTimeStamp        = 28;
+    public const byte IndexGuidLanguage         = 29;
+    public const byte IndexGuidTransparency     = 30;
+    public const byte IndexGuidCurveFittingError    = 31;
+    public const byte IndexGuidRecoLattice          = 32;
+    public const byte IndexGuidCursorDown           = 33;
+    public const byte IndexGuidSecondaryTipSwitch   = 34;
+    public const byte IndexGuidBarrelDown   = 35;
+    public const byte IndexGuidTabletPick   = 36;
+    public const byte IndexGuidRop          = 37;
+    public const byte IndexGuidMax          = 37;
+}
